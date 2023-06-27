b945c637e5bb9be3bfb7f0043e436f1a03d8bcdb
yashwanth lal <yashwanthlal9676@gmail.com>
2023-06-27 11:43:09 +0530

    resolved few errors

---
+++ movieMiniTestCases2/src/components/ProtectedRoute/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect, Route} from 'react-router-dom'
+import './index.css'
+
+class ProtectedRoute extends Component {
+  render() {
+    const jwtToken = Cookies.get('jwt_token')
+
+    if (jwtToken === undefined) {
+      return <Redirect to="/login" />
+    }
+
+    return <Route {...this.props} />
+  }
+}
+export default ProtectedRoute
